4c59d1fc5a216feb4fb3ea1e32bfa0ccdb605fb7
Luan Pham <phluan92@gmail.com>
2017-04-14 18:39:21 +0700

    apply database-cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,7 +33,27 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.start
+  end
+
+  # These specs run with Capybara's Selenium driver to support JavaScript. This driver
+  # run in different process to the process of spec itself so it can not access to
+  # current database transaction
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
