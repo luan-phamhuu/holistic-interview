4448207811e69bf32275281f0c4e8dc8b3c2a038
Luan Pham <phluan92@gmail.com>
2017-04-14 18:40:30 +0700

    clear cache between test cases

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,6 +55,11 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 
+  # Clear Rails caching
+  config.before(:each) do
+    Rails.cache.clear
+  end
+
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
